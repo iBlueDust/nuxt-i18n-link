091cc29511b40c8d844bf3e55369023d6768dd71
Peter R. Raozen <peterraozen@gmail.com>
2020-12-21 11:52:37 +0700

    Fixed module for static site builds
    Static builds will not import the module as it is considered as SSR, so <nuxt-i18n-link> will appear in production builds instead of <a>.
    
    21/12/2020
    
    21/12/2020

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import path from 'path'
 
 export default function nuxtI18nLink() {
-	this.addPlugin({
-		src: path.resolve(__dirname, 'plugin.js'),
-		ssr: false,
-		filename: 'nuxt-i18n-link.js',
-	})
+	this.addPlugin(path.resolve(__dirname, 'plugin.js'))
 }
 
 export { default as meta } from '../package.json'
